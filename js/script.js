00bcd8805534afac223d7027d5011976e7374c8b
Abulxayrxon <132061576+Abulhayrhon@users.noreply.github.com>
2023-06-06 18:11:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// 1
+// arr.pop()
+// oxiridan bitta oladi
+
+// function pop(arr) {
+//     if (arr.length === 0) {
+//       return undefined;
+//     } else {
+//       let noteArr = arr[arr.length - 1];
+//       let newArr = arr.slice(0, -1);
+//       return [newArr, noteArr];
+//     }
+//   }
+
+
+// ###################################################################
+
+// 2
+// arr.shift()
+// oldidan bitta oladi
+
+// function shift(arr) {
+//     if (arr.length === 0) {
+//       return undefined;
+//     } else {
+//       let nanArr = arr[0];
+//       arr.splice(0, 1);
+//       return firstElement;
+//     }
+//   }
+
+// ###################################################################
+
+// 3
+// arr.unshift()
+// oldidan qushadi
+
+
+// ###################################################################
+
+// 4
+// str.split()
+
+
+// #####################################################################
+
+// 6
+// arr.push()
+
+// function ArrPush(arr,newItem){
+//   arr[arr.length]=newItem
+//   return arr
+// }
+// console.log(ArrPush([1,2,3,4,"salom",8],"hello"))
+
+// #################################################################
